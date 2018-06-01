6104f562e8df704c2eecc1bc8c0d4a1d54aff5bc
装神 <ms_test@qq.com>
2018-06-01 09:44:42 +0800

    修改端口号问题

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: "/",
     proxyTable: {
       "/api/": {
-        target: "http://test.z9168.com:3200",
+        target: "http://test.z9168.com",
         changeOrigin: true
       }
     },
